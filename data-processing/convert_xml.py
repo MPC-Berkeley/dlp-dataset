98dcff6af201fec52082e26ac419db0136dfcf05
Michelle Pan <michellepan8@gmail.com>
2021-05-05 16:40:32 -0700

    add progress bars to scripts

--- data-processing/convert_xml.py
+++ data-processing/convert_xml.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import uuid
 import json
+from tqdm import tqdm
 import xml.etree.ElementTree as ET 
 
 import pandas as pd 
@@ -13,19 +14,18 @@ def gen_token():
 
 parser = argparse.ArgumentParser()
 parser.add_argument('file', help='XML file to convert')
-parser.add_argument('-v', '--verbose', help='increase output verbosity', action='store_true')
 args = parser.parse_args()
 
 tree = ET.parse(args.file)
 root = tree.getroot()
 
-if args.verbose:
-    print('finished loading XML')
+print('finished loading XML')
 
 trajectory_df = pd.DataFrame()
 filename_stem = os.path.splitext(args.file)[0]
 
-for f in range(len(root)):
+print('writing frames')
+for f in tqdm(range(len(root))):
 
     frame = root[f]
     if len(frame) < 2:
@@ -40,8 +40,5 @@ for f in range(len(root)):
     if f % 100 == 0:
         trajectory_df.to_csv(filename_stem + '.csv', mode=('a' if f else 'w'), encoding='utf-8', index=False, header=(not f))
         trajectory_df = pd.DataFrame()
-
-        if args.verbose:
-            print('finished frame', f)
 
 trajectory_df.to_csv(filename_stem + '.csv', mode='a', encoding='utf-8', index=False, header=False)
--- data-processing/generate_tokens.py
+++ data-processing/generate_tokens.py
@@ -3,26 +3,28 @@ import hashlib
 import json
 import ntpath
 import os
+import subprocess
+from tqdm import tqdm
 import pandas as pd
 import numpy as np
 
-def gen_token(str):
-    return hashlib.sha1(str.encode('utf-8')).hexdigest()
+def gen_token(key):
+    return hashlib.sha1(key.encode('utf-8')).hexdigest()
 
 def write_frames():
     frames = {}
     frame_ids = np.unique(df['frame_id'].to_numpy()).astype(int)
 
-    for idx in frame_ids:
-        frame_df = df[df['frame_id'] == str(idx)]
+    for frame_id in tqdm(frame_ids):
+        frame_df = df[df['frame_id'] == frame_id]
         agent_ids = frame_df['id'].to_numpy().astype(int)
         frame = {
-            'frame_token': gen_token('{}_frame_{}'.format(filename_base, idx)),
+            'frame_token': gen_token('{}_frame_{}'.format(filename_base, frame_id)),
             'scene_token': scene_token,
             'timestamp': frame_df['timestamp'].iloc[0],
-            'instances': [gen_token('{}_instance_{}_{}'.format(filename_base, idx, x)) for x in agent_ids],
-            'prev_frame': '' if idx == 0 else gen_token('{}_frame_{}'.format(filename_base, idx - 1)),
-            'next_frame': '' if idx == max(frame_ids) else gen_token('{}_frame_{}'.format(filename_base, idx + 1))
+            'instances': [gen_token('{}_instance_{}_{}'.format(filename_base, frame_id, agent_id)) for agent_id in agent_ids],
+            'prev_frame': '' if frame_id == 0 else gen_token('{}_frame_{}'.format(filename_base, frame_id - 1)),
+            'next_frame': '' if frame_id == max(frame_ids) else gen_token('{}_frame_{}'.format(filename_base, frame_id + 1))
         }
         frames[frame['frame_token']] = frame
 
@@ -33,16 +35,16 @@ def write_agents():
     agents = {}
     agent_ids = np.unique(df['id'].to_numpy())
 
-    for idx in agent_ids:
-        agent_df = df[df['id'] == idx]
+    for agent_id in tqdm(agent_ids):
+        agent_df = df[df['id'] == agent_id]
         frame_ids = agent_df['frame_id'].to_numpy().astype(int)
         agent = {
-            'agent_token': gen_token('{}_agent_{}'.format(filename_base, idx)),
+            'agent_token': gen_token('{}_agent_{}'.format(filename_base, agent_id)),
             'scene_token': scene_token,
             'type': agent_df['type'].iloc[0],
             'size': [agent_df['length'].iloc[0], agent_df['width'].iloc[0]],
-            'first_instance': gen_token('{}_instance_{}_{}'.format(filename_base, min(frame_ids), idx)),
-            'last_instance': gen_token('{}_instance_{}_{}'.format(filename_base, max(frame_ids), idx))
+            'first_instance': gen_token('{}_instance_{}_{}'.format(filename_base, min(frame_ids), agent_id)),
+            'last_instance': gen_token('{}_instance_{}_{}'.format(filename_base, max(frame_ids), agent_id))
         }
         agents[agent['agent_token']] = agent
     
@@ -51,23 +53,27 @@ def write_agents():
 
 def write_instances():
     instances = {}
+    agent_ids = np.unique(df['id'].to_numpy())
 
-    for _, row in df.iterrows():
-        frame_id, agent_id = row['frame_id'], row['id']
-        agent_frame_ids = df[df['id'] == agent_id]['frame_id'].to_numpy().astype(int)
-        instance = {
-            'instance_token': gen_token('{}_instance_{}_{}'.format(filename_base, frame_id, agent_id)),
-            'agent_token': gen_token('{}_agent_{}'.format(filename_base, agent_id)),
-            'frame_token': gen_token('{}_frame_{}'.format(filename_base, frame_id)),
-            'coords': [row['utm_x'], row['utm_y']],
-            'heading': row['utm_angle'],
-            'speed': row['speed'],
-            'acceleration': [row['lateral_acceleration'], row['tangential_acceleration']],
-            'mode': '',
-            'prev': '' if frame_id == min(agent_frame_ids) else gen_token('{}_instance_{}_{}'.format(filename_base, frame_id - 1, agent_id)),
-            'next': '' if frame_id == max(agent_frame_ids) else gen_token('{}_instance_{}_{}'.format(filename_base, frame_id - 1, agent_id))
-        }
-        instances[instance['instance_token']] = instance
+    for agent_id in tqdm(agent_ids):
+        agent_df = df[df['id'] == agent_id]
+        frame_ids = agent_df['frame_id'].to_numpy().astype(int)
+
+        for i, row in agent_df.iterrows():
+            frame_id = row['frame_id']
+            instance = {
+                'instance_token': gen_token('{}_instance_{}_{}'.format(filename_base, frame_id, agent_id)),
+                'agent_token': gen_token('{}_agent_{}'.format(filename_base, agent_id)),
+                'frame_token': gen_token('{}_frame_{}'.format(filename_base, frame_id)),
+                'coords': [row['utm_x'], row['utm_y']],
+                'heading': row['utm_angle'],
+                'speed': row['speed'],
+                'acceleration': [row['lateral_acceleration'], row['tangential_acceleration']],
+                'mode': '',
+                'prev': '' if frame_id == min(frame_ids) else gen_token('{}_instance_{}_{}'.format(filename_base, frame_id - 1, agent_id)),
+                'next': '' if frame_id == max(frame_ids) else gen_token('{}_instance_{}_{}'.format(filename_base, frame_id + 1, agent_id))
+            }
+            instances[instance['instance_token']] = instance
 
     with open(filename_stem + '_instances.json', 'w') as out:
         json.dump(instances, out)
@@ -81,7 +87,7 @@ def write_scene():
         'filename': filename_base,
         'first_frame': gen_token('{}_frame_{}'.format(filename_base, min(frame_ids))),
         'last_frame': gen_token('{}_frame_{}'.format(filename_base, max(frame_ids))),
-        'agents': [gen_token('{}_agent_{}'.format(filename_base, idx)) for idx in agent_ids]
+        'agents': [gen_token('{}_agent_{}'.format(filename_base, agent_id)) for agent_id in agent_ids]
     }
 
     with open(filename_stem + '_scene.json', 'w') as out:
@@ -96,11 +102,21 @@ if __name__ == '__main__':
 
     filename_stem = os.path.splitext(args.file)[0]
     filename_base = os.path.splitext(ntpath.basename(args.file))[0]
+
+    process = subprocess.Popen(['mediainfo', filename_stem + '.MOV'], stdout=subprocess.PIPE)
+    output, _ = process.communicate()
     scene_token = gen_token(filename_base)
 
     df = pd.read_csv(args.file)
 
+    print('writing frames')
     write_frames()
+
+    print('writing agents')
     write_agents()
+
+    print('writing instances')
     write_instances()
+
+    print('writing scene')
     write_scene()
