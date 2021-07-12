4baf28be6b07d4419c619062b802f64fa4637098
XuShenLZ <xushen0118@outlook.com>
2021-07-11 19:32:15 -0700

    add pillow

--- setup.py
+++ setup.py
@@ -8,11 +8,12 @@ setuptools.setup(
     author_email='xu_shen@berkeley.edu',
     packages=['dlp'],
     install_requires=[
-    "matplotlib==3.4.1",
-    "numpy==1.20.2",
-    "pandas==1.2.4",
-    "tqdm==4.60.0",
-    "pyyaml==5.4.1",
-    "jupyterlab==3.0.16"
+        "pillow=8.3.1",
+        "matplotlib==3.4.1",
+        "numpy==1.20.2",
+        "pandas==1.2.4",
+        "tqdm==4.60.0",
+        "pyyaml==5.4.1",
+        "jupyterlab==3.0.16"
     ]
 )
