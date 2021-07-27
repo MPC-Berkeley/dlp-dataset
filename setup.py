7e3e9ffd57afcf9753819e71b84268a954b41360
XuShenLZ <xushen0118@outlook.com>
2021-07-27 12:38:08 -0700

    small fix in setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     author_email='xu_shen@berkeley.edu',
     packages=['dlp'],
     install_requires=[
-        "pillow=8.3.1",
+        "pillow==8.3.1",
         "matplotlib==3.4.1",
         "numpy==1.20.2",
         "pandas==1.2.4",
