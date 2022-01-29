c7def8a61783fb91b70b17a38d03f2b20564852b
XuShenLZ <xushen0118@outlook.com>
2022-01-28 18:56:55 -0800

    relaxed the dependency version requirement

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ setuptools.setup(
     author_email='xu_shen@berkeley.edu',
     packages=['dlp'],
     install_requires=[
-        "pillow==9.0.0",
-        "matplotlib==3.4.1",
-        "numpy==1.21.0",
-        "pandas==1.2.4",
-        "tqdm==4.60.0",
-        "pyyaml==5.4.1",
-        "jupyterlab==3.0.17"
+        "pillow>=9.0.0",
+        "matplotlib>=3.4.1",
+        "numpy>=1.21.0",
+        "pandas>=1.2.4",
+        "tqdm>=4.60.0",
+        "pyyaml>=5.4.1",
+        "jupyterlab>=3.0.17"
     ]
 )
